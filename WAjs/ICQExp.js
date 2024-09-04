770b1e0a59ce064275a9addcd13a8a5bd6d33584
Roman Zeldinov <143893762+WowInceptionGood@users.noreply.github.com>
2024-09-04 14:47:19 +0000

    right buddies list

--- WAjs/ICQExp.js
+++ WAjs/ICQExp.js
@@ -16,7 +16,7 @@ var oscarAPI = "http://api.oscar.nina.bz";
 
 // -------------------------------------------------
 // Internal Variables
-// WARNING! DO NOT MODIFY!]
+// WARNING! DO NOT MODIFY!
 
 // Login Info
 var login;
@@ -43,7 +43,7 @@ var buddiesList;
 
 var buddies;
 
-// Integration with WebAgent
+// Integration with WebAgent (TM)
 var currentConversation;
 
 
@@ -77,7 +77,7 @@ function makeSessionKey(){
 }
 
 function startSession(){
-	var data = "f=JSON&k=n1n4FcOouKVTJf11&a=".concat(aToken).concat("&events=im,buddylist");
+	var data = "f=JSON&k=n1n4FcOouKVTJf11&a=".concat(aToken).concat("&events=im");
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', screennameAPI.concat("/aim/startSession").concat("?").concat(data), false);
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -101,21 +101,22 @@ function fetchEvents(){
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	xhr.send(null);
 	if (xhr.status === 200) {
+		//console.log(xhr.responseText);
 		var respObj = JSON.parse(xhr.responseText);
 		currentEventsURL = respObj.response.data.fetchBaseURL;
 		respObj.response.data.events.forEach(function (eventItem) {
 			if (eventItem.type === "buddylist"){
 				if (lastFetch === 0){
-					buddiesList = eventItem.eventData.groups[0].buddies;
+					getPresence();
 					lastFetch = 1;
 				}
 			}
-			else if (eventItem.type === "im"){
+			if (eventItem.type === "im"){
 				if (eventItem.eventData.source.aimId === currentConversation.toString()){
 					appendConvo(currentConversation, eventItem.eventData.message);
 				}
 				else{
-					gotMessage();
+					console.log("OOU PRISHLO SMSKA DAVAY INCEPTION DELAY NORM ZVUK A TO ETO HUYNAY");
 				}
 			}
 			prevEvN = eventItem.seqNum;
@@ -127,42 +128,6 @@ function fetchEvents(){
 		return 0;
 	}
 }
-async function afetchEvents(){
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', currentEventsURL.concat("&f=JSON"), true);
-	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
- 
-	xhr.onload = function() {
-	   if (xhr.status === 200) {
-		  console.log(xhr.responseText);
-		  var respObj = JSON.parse(xhr.responseText);
-		  currentEventsURL = respObj.response.data.fetchBaseURL;
-		  respObj.response.data.events.forEach(function (eventItem) {
-			 if (eventItem.type === "buddylist"){
-				if (lastFetch === 0){
-				   buddiesList = eventItem.eventData.groups[0].buddies;
-				   lastFetch = 1;
-				}
-			 }
-			 else if (eventItem.type === "im"){
-				if (eventItem.eventData.source.aimId === currentConversation.toString()){
-				   appendConvo(currentConversation, eventItem.eventData.message);
-				}
-				else{
-				 gotMessage();
-				}
-			 }
-			 prevEvN = eventItem.seqNum;
-		  });
-		  return 1;
-	   } else {
-		  throw new Error('Request failed: ' + xhr.statusText);
-		  return 0;
-	   }
-	};
- 
-	xhr.send(null);
- }
 
 function getConsent(){
 	var data = "f=JSON&devId=n1n4FcOouKVTJf11&enc=".concat(sessionKey);
@@ -194,34 +159,24 @@ function getInfo(){
 	}	
 }
 
-function sendIM(destination, message) {
-    var data = "f=JSON"
-        .concat("&k=n1n4FcOouKVTJf11")
-        .concat("&ts=").concat(Math.floor(new Date().getTime() / 1000))
-        .concat("&sig_sha256=").concat(sessionKey)
-        .concat("&t=").concat(destination)
-        .concat("&message=").concat(encodeURIComponent(message))
-        .concat("&a=").concat(aToken)
-        .concat("&aimsid=").concat(aimsid);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "http://api.oscar.nina.bz/im/sendIM?" + data, false);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send(null);
-
-    if (xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-        if (response.response.statusCode === 200) {
-            appendConvo(login, message);
-            return 1;
-        }
-    } else {
-        throw new Error('Request failed: ' + xhr.statusText);
-    }
-    return 0;
+function sendIM(destination, message){
+	var data = "f=JSON&k=n1n4FcOouKVTJf11".concat("&ts=").concat(new Date().getTime() / 1000).concat("&sig_sha256=").concat(sessionKey).concat("&t=").concat(destination).concat("&message=").concat(message).concat("&a=").concat(aToken).concat("&aimsid=").concat(aimsid);
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', oscarAPI.concat("/im/sendIM").concat("?").concat(data), false);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send(null);
+	if (xhr.status === 200) {
+		if (JSON.parse(xhr.responseText).response.statusCode === 200){
+			appendConvo(login, message);
+		}
+		return 1;
+	} else {
+		throw new Error('Request failed: ' + xhr.statusText);
+		return 0;
+	}	
 }
 
-function getConsent(){
+function deleteEvent(){
 	var data = "f=JSON&aimsid=".concat(aimsid).concat("&eventNumber=").concat(prevEvN);
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', screennameAPI.concat("/aim/deleteEvents").concat("?").concat(data), false);
@@ -236,7 +191,18 @@ function getConsent(){
 	}	
 }
 
-function gotMessage(){
-	var audio = new Audio('res/music/icq.mp3');
-	audio.play();
+function getPresence(){
+	var data = "f=JSON&aimsid=".concat(aimsid).concat("&bl=true")
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', oscarAPI.concat("/presence/get").concat("?").concat(data), false);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send(null);
+	if (xhr.status === 200) {
+		var respObj = JSON.parse(xhr.responseText);
+		buddiesList = respObj.response.data.groups[0].buddies;
+		return 1;
+	} else {
+		throw new Error('Request failed: ' + xhr.statusText);
+		return 0;
+	}
 }
